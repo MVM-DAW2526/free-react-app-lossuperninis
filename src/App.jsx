35b0912a5dc843259d4474629a44c56514f28aca
Fran <ffernandez@institutmvm.cat>
2026-05-13 16:33:14 +0200

    HTML/CSS hecho por RUBEN. Organizado por Francisco

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,27 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>base html</h1>
-    </>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pokemon-Web</title>
+    <link rel="stylesheet" href="pokemon.css">
+</head>
+<body>
+    <header class="encabezado">
+        <img class="logo" src="hero-pokeball-3430739968171e9fe85357e4739be704.png" alt="Logo de la web de una pokeball" style="width: 40px;">
+        <button class="boton-titulo"><a class="titulo" href="pokemon.html">PokeWeb</a></button>
+        <nav class="enlaces">
+            <ul>
+                <li class="lista"><a class="enlace" href="details.html">Detalles</a></li>
+            </ul> 
+        </nav>
+        <button class="boton-user"><img class="logo-user" src="comic-style-user-icon-with-transparent-background-file-png.webp" alt="Imagen del logo del usuario" style="width: 75px;"></button>
+    </header>
+</body>
+</html>
   )
 }
 
