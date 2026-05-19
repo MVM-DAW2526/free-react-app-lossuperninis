e7b40da53b4f138a6a3c21bddfe4d327999ca19a
Fran <ffernandez@institutmvm.cat>
2026-05-19 12:09:51 +0200

    cambios css encabezado

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
 export const Header = ({ logo, userIcon }: any) => (
   <header className="encabezado">
-    <img src={logo} alt="Logo" width={40} />
+    <img src={logo} alt="Logo" width={50} height={50} className="logo"/>
     <h2 className="titulo">PokeWeb</h2>
-    <button className="boton-titulo"><a className="titulo" href="/">Inicio</a></button>
+    <button className="titulo-centrado"><a className="enlace" href="/">Inicio</a></button>
     <button className="boton-user"><img src={userIcon} alt="User" width={75} /></button>
   </header>
 );
